076f2f714e2a17f03ad74a2266df03f8f9697a99
sudakasudo <51377700+sudakasudo@users.noreply.github.com>
2020-10-10 23:27:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        employee e = new employee();
+        e.details();
+
+        employee m = new employee();
+        m.details();
+
+
+    }
+}
---
+++ employee.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class employee {
+
+    public void details()
+    {
+        int a=23;
+        String b="Saurab";
+        String c="chennai";
+        System.out.println("The name is " +b);
+        System.out.println("The age is " +a);
+        System.out.println("The city is " +c);
+
+    }
+}
